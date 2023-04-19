da62007b97304b8ac8cf0294ade309738aee3775
A.Ramadan.ext <libgdxengine@gmail.com>
2023-04-19 20:51:27 +0200

    make PRODUCTION

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     APP_NAME: "لتسكنوا",
     API_DEVELOPMENT: "http://localhost:8000",
     API_PRODUCTION: "https://letaskono-api-yxpn.onrender.com/",
-    PRODUCTION: false,
+    PRODUCTION: true,
     DOMAIN_DEVELOPMENT: "http://localhost:3000",
     DOMAIN_PRODUCTION: "https://letaskono.vercel.app/",
     FB_APP_ID: "6217301571635680",
