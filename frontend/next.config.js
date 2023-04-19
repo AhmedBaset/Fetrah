4fb2194d9b42c498003c4335a29fca6e950a2660
A.Ramadan.ext <libgdxengine@gmail.com>
2023-04-19 20:57:23 +0200

    fix slash

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   publicRuntimeConfig: {
     APP_NAME: "لتسكنوا",
     API_DEVELOPMENT: "http://localhost:8000",
-    API_PRODUCTION: "https://letaskono-api-yxpn.onrender.com/",
+    API_PRODUCTION: "https://letaskono-api-yxpn.onrender.com",
     PRODUCTION: true,
     DOMAIN_DEVELOPMENT: "http://localhost:3000",
     DOMAIN_PRODUCTION: "https://letaskono.vercel.app/",
